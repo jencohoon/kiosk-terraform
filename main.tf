1f745f24b0b48f3da2b31e1efb5856c5eb86be1d
JenC <jzinner@gmail.com>
2025-01-02 13:17:09 -0600

    setup remote-state folder to setup s3 and DynamoDB table without terraform depencies

--- main.tf
+++ main.tf
@@ -1,13 +1,29 @@
 terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
   backend "s3" {
-    bucket         = "kiosk-tf-s3-bucket" # Match the S3 bucket name created in backend.tf
-    key            = "terraform/state/terraform.tfstate" # Path in the bucket to store the state file
-    region         = "us-east-1" # Region where the bucket is located
-    dynamodb_table = "kiosk-terraform-state-lock" # Match the DynamoDB table name created in backend.tf
+    # Match the S3 bucket name already created in backend.tf
+    bucket = "kiosk-tf-s3-bucket-jen"
+    # Path in the bucket to store the state file
+    key = "terraform/state/terraform.tfstate"
+    # Region where the bucket is located
+    region = "us-east-1"
+    # Match the DynamoDB table name created in backend.tf
+    dynamodb_table = "kiosk-terraform-state-lock"
     encrypt        = true
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
 
 
 resource "aws_instance" "tf_instance" {
